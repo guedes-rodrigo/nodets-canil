04766ca0a13014da603501f04bfb926fd65da101
guedes-rodrigo <guedes_33@hotmail.com>
2024-02-10 11:50:18 -0300

    Config routes and controllers

--- src/server.ts
+++ src/server.ts
@@ -2,18 +2,25 @@ import express from "express";
 import dotenv from "dotenv";
 import mustache from "mustache-express";
 import path from "path";
-import { Server } from "http";
+import mainRoutes from "./routes/index";
 
 dotenv.config();
 
 const server = express();
 
 server.set("view engine", "mustache");
-server.set("view", path.join(__dirname, "views"));
+server.set("views", path.join(__dirname, "views"));
 server.engine("mustache", mustache());
 
 server.use(express.static(path.join(__dirname, "../public")));
 
 //Rotas
+server.use(mainRoutes);
 
-server.listen(process.env.PORT);
+server.use((req, res) => {
+  res.send("Página não encontrada");
+});
+
+server.listen(process.env.PORT, () => {
+  console.log(`Servidor rodando na porta ${process.env.PORT}`);
+});
